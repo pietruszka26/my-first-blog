ec1467181aa9fdf82785264d3facd9ebe2bf0736
pietruszka26 <pietruszka26@wp.pl>
2016-10-22 15:41:59 +0200

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,22 @@
+# print('Hello, Django girls!')
+# if 3>2:
+    # print('To działa')
+# if 5>2:
+    # print('5 jest wieksze od 2')
+# else:
+    # print('5 nie jest wieksze')
+# name = 'Sonja'
+# if name == 'Magda':
+    # print ('Hej Magda!')
+# elif name == 'Sonja':
+    # print ('Hej Sonja!')
+# else:
+    # print('Hej nieznajoma!')
+# def hej(imie):
+#         print('Hej' + imie + '!')
+# dziewczyny = ['Reachel' , 'Monica' , 'Phoebe' , 'Ola' , 'Magda']
+# for imie in dziewczyny:
+#     hej(imie)
+#     print('Kolejna dziewczyna')
+for i in range (1,6) :
+    print(i)
